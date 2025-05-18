563678b54d195c54765788235c0a51d816102ba6
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-05-18 16:06:21 +0200

    fix datacare postgresql dependecy resolutions

--- derivation.nix
+++ derivation.nix
@@ -1,27 +1,42 @@
-{ buildPackage, lib, pkg-config, cmake, protobuf, zlib, openssl, libpq, writeScriptBin}:
+{ buildPackage, stdenv, lib, pkg-config, cmake, protobuf, zlib, openssl, postgresql }:
+
 let
-datacare = buildPackage {
-  pname = "datacare";
-  version = "0.3.1";
+  datacare = buildPackage {
+    pname = "datacare";
+    version = "0.3.1";
 
-  src = ./.;
+    src = ./.;
 
-  cargoSha256 = lib.fakeSha256;
+    cargoSha256 = lib.fakeSha256;
 
-  nativeBuildInputs = [ pkg-config cmake protobuf zlib openssl libpq ];
+    nativeBuildInputs = [ pkg-config cmake ];
+    buildInputs = [ protobuf zlib openssl postgresql ];
 
-  preBuildHook = ''
-  '';
-
-  meta = {
-    description = "Simple rust server which manages users, stations and regions";
-    homepage = "https://github.com/tlm-solutions/datacare";
+    meta = {
+      description = "Simple rust server which manages users, stations and regions";
+      homepage = "https://github.com/tlm-solutions/datacare";
+    };
   };
-};
-in 
-(
-  writeScriptBin "datacare" ''
-    export LD_LIBRARY_PATH=${libpq}/lib:$LD_LIBRARY_PATH
-    ${datacare}/bin/datacare $@
-  ''
-)
+in
+stdenv.mkDerivation {
+  name = "datacare-patchelf";
+  src = datacare;
+
+  buildPhase = "";
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src/bin/datacare $out/bin/datacare
+    chmod +w $out/bin/datacare
+  
+    patchelf --replace-needed libpq.so.5 ${postgresql.lib}/lib/libpq.so $out/bin/datacare
+
+    # check if the patch succeded and exit if a depedency is not found
+    local patch_succeded
+
+    patch_succeded=$(ldd $out/bin/datacare | grep "not found" | wc -l || true)
+    if [[ "$patch_succeded" -ne 0 ]]; then
+      echo "Patching failed" && exit 1
+    fi
+  '';
+}
